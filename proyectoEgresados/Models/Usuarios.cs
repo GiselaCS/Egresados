fd9da0a749db37730da4f2f3346da8e4a2c64eae
GiselaCS <giselacriollo16@gmail.com>
2021-03-20 13:43:43 -0500

    se añadieron los campos obligatorios en la clase usuario

--- proyectoEgresados/Models/Usuarios.cs
+++ proyectoEgresados/Models/Usuarios.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,21 +9,51 @@ namespace proyectoEgresados.Models
     public class Usuarios
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        
         public int Documento { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public string Tipodoc { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public int Celular { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [EmailAddress(ErrorMessage = "Debe ingresar un mail valido")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [RegularExpression("[MmFfOo]", ErrorMessage = "Solo puede ingresar una M o F")]
         public string Genero { get; set; }
+
         public bool Aprendiz { get; set; }
+
         public bool Egresado { get; set; }
 
+        [Required(ErrorMessage = "Campo obligatorio")]
         public string Areaformacion { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public DateTime Fechaegresado { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public string Barrio { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public string Ciudad { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public string Departamento { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
         public DateTime Fecharegistro { get; set; }
         
     }
